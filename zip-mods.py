3eedf0ef3e561ac049cf532e36ce3b613ec5c808
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-11-30 03:12:29 -0700

    Update zip-mods.py

--- zip-mods.py
+++ zip-mods.py
@@ -12,7 +12,7 @@ MAC_DEST = "~/Library/Application Support/Founder of the Final Cult/mods/"
 
 def main():
   if len(sys.argv) < 2:
-    print("Usage: zip-mods.py <mod-directory>")
+    print("Usage: zip-mods.py <mod-directory-1, mod-directory-2, ...>")
     return
   
   system = platform.system()
@@ -26,22 +26,27 @@ def main():
     print(f"Unsupported OS: {system}")
     return
 
+  # Create zip archives for each mod directory
   mod_dirs = sys.argv[1:]
   for i in range(len(mod_dirs)):
     mod_dir = mod_dirs[i]
     print(f"Zipping mod directory: {mod_dir}")
 
     if system == "Windows":
+      # Use PowerShell Compress-Archive cmdlet on Windows
       subprocess.run(["powershell", "Compress-Archive", "-Path", mod_dir, "-DestinationPath", f"{mod_dir}.zip"])
     else:
+      # Use zip command on Linux and macOS
       subprocess.run(["zip", "-r", f"{mod_dir}.zip", mod_dir])
-
+    
+    # Delete existing zip at destination if it exists
     if os.path.exists(f"{dest}{mod_dir}.zip"):
       os.remove(f"{dest}{mod_dir}.zip")
     
+    # Move the created zip to the destination directory
     shutil.move(f"{mod_dir}.zip", dest)
+
     print(f"Created zip archive at {dest}{mod_dir}.zip")
-    
     if i < len(mod_dirs) - 1:
       print("")
 
