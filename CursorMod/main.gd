1dbfa7eb895580b85163ea2ff9a0c838ce8e751b
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-11-30 03:10:39 -0700

    Fixed loading

--- CursorMod/main.gd
+++ CursorMod/main.gd
@@ -4,7 +4,9 @@ extends BaseMod
 func init_mod() -> void:
     print("Init cursor mod now")
     
-    var cursor_texture: Texture2D = load("res://CursorMod/cursor.png")
+    # Godot will throw an error when using load() to load this image since it is in a mod
+    # This still throws a warning but that is fine
+    var cursor_texture: Image = Image.load_from_file("res://CursorMod/cursor.png")
     Input.set_custom_mouse_cursor(cursor_texture)
     
     for _i: int in 3:
