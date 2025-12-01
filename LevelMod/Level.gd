c9cbaf25c769bdbacdb9eb45fbe133e5bf483ec6
Inderpreet Dhillon <4023685+Inderpreet-D@users.noreply.github.com>
2025-12-01 09:16:19 -0700

    Tweaked spawning

--- LevelMod/Level.gd
+++ LevelMod/Level.gd
@@ -2,9 +2,18 @@ extends Level
 
 
 func _update_config_waves(config: LevelConfig) -> void :
-    const pool_1: Array[String] = ["FishKeeper", "Warrior", "SpearThrower"]
-    const pool_2: Array[String] = ["Worshipper", "Shaman"]
-    const pool_3: Array[String] = ["Congregation", "Pope"]
+    const pool_1: Array[String] = [
+        "Pigeon", "Crow", "Kestrel", "Hawk",
+        "Cop", "Baton", "FBI", "CIA"
+    ]
+    const pool_2: Array[String] = [
+        "FishKeeper", "Worshipper", "Congregation", "Pope",
+        "Weeb1", "Weeb2", "Idol", "Ex1", "Ex2", "Ex3", "Ex4", "Generic1", "Generic2", "Generic3"
+    ]
+    const pool_3: Array[String] = [
+        "Warrior", "Shaman", "SpearThrower",
+        "FireTrainee", "WindMagician", "EarthWizard"
+    ]
     const base_unit_seconds: int = 60
     const wave_time: int = base_unit_seconds * 2
     const boss_time: int = base_unit_seconds * 1
