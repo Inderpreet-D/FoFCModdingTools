ff548c06bd88eb3d4db2011b987d0f2beb3f414a
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-02 10:07:26 -0700

    Added pickup mod

---
+++ PickupMod/energy_pickup/EnergyPickup.gd
@@ -0,0 +1,21 @@
+extends Pickup
+
+
+func reset() -> void :
+    super.reset()
+
+    value = 1.0
+    speed = 500.0
+    catch_squared_distance = 16.0
+
+
+func on_pickup() -> void :
+    super.on_pickup()
+    
+    prints("Picked up energy")
+    
+    for player: Player in Utilities.get_all_players():
+        for weapon: Weapon in player.get_weapon_nodes():
+            for _i: int in 10:
+                weapon.attack()
+                await Utilities.wait(0.1)
--- PickupMod/main.gd
+++ PickupMod/main.gd
@@ -1,5 +1,21 @@
 extends BaseMod
 
 
+const ENERGY_KEY: String = "energy_pickup"
+
+
 func init_mod() -> void:
     prints("Init pickup mod")
+    
+    NodePool.scenes[ENERGY_KEY] = load("res://PickupMod/energy_pickup/EnergyPickup.tscn")
+    
+    PickupHandler.drop_handlers.append(func(enemy: Enemy) -> Pickup:
+        var can_spawn: bool = randf_range(0.0, 100.0) <= Stats.luck * 5.0
+        if not can_spawn:
+            return null
+        
+        print("Dropped energy")
+        var energy_pickup: Pickup = NodePool.create_node(ENERGY_KEY)
+        energy_pickup.global_position = enemy.global_position
+        return energy_pickup
+    )
