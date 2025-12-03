e6e281f62283ac0b7e0ad278c641f06ab33b9241
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-03 11:04:36 -0700

    Added sound mod

---
+++ SoundMod/main.gd
@@ -0,0 +1,12 @@
+extends BaseMod
+
+
+func init_mod() -> void:
+    print("Init sound mod now")
+    
+    AudioManager.add_sound("Priority", "mod/pop", -10.0, "res://SoundMod/pop-423717.mp3")
+    
+    EventBus.pickup_collected.connect(func(_pickup: Pickup) -> void:
+        print('Playing mod sound')
+        AudioManager.play("mod/pop")
+    )
