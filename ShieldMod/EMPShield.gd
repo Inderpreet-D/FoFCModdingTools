f165f0782629051a53433381a28b1ee80f158d13
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-03 20:05:07 -0700

    Added shield mod

---
+++ ShieldMod/EMPShield.gd
@@ -0,0 +1,34 @@
+class_name EMPShield extends Shield
+
+
+func _init() -> void :
+    type = "EMPShield"
+
+    super._init(ShieldStat._2)
+
+
+func on_break() -> void :
+    super.on_break()
+    
+    # Async func that will be called and done after the shield is un-equipped
+    # await won't work in on_break function for shields (by design)
+    var break_func: Callable = func() -> void:
+        for node: Node in EnemySpawner.get_children():
+            if node is not EnemyBullet:
+                continue
+            
+            var bullet: EnemyBullet = node
+            bullet.speed = 0.0
+            bullet.damage = 0.0
+            bullet.rotation_speed = 10.0
+
+        for node: Node in EnemySpawner.get_children():
+            if node is not EnemyBullet:
+                continue
+            
+            var bullet: EnemyBullet = node
+            if bullet.speed < 1.0:
+                bullet._start_dying()
+                await Utilities.wait(0.1)
+    
+    break_func.call()
---
+++ ShieldMod/main.gd
@@ -0,0 +1,16 @@
+extends BaseMod
+
+
+func init_mod() -> void:
+    print("Init shield mod now")
+    
+    var def: ShieldDef = ShieldDef.new({
+        "description": "Stops projectiles when <broken>",
+        "icon": "res://ShieldMod/sprite.png",
+        "key": "EMPShield",
+        "tier": 1,
+        "title": "EMP Shield"
+    })
+    DefHandler.add_shield(def)
+    
+    ClassRegistry.register("EMPShield", "res://ShieldMod/EMPShield.gd")
