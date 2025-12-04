16f00f4650954052a50b4d190b828bdd817ea205
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-03 20:27:00 -0700

    Tweaked world boss passive

--- PassiveMod/boss_world_passive/stop_node/StopNode.gd
+++ PassiveMod/boss_world_passive/stop_node/StopNode.gd
@@ -5,7 +5,29 @@ extends Node2D
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 
 
+func _stop_bullets() -> void:
+    for node: Node in EnemySpawner.get_children():
+        if node is not EnemyBullet:
+            continue
+
+        var bullet: EnemyBullet = node
+        bullet.speed = 0.0
+        bullet.damage = 0.0
+        bullet.rotation_speed = 0.0
+
+    for node: Node in EnemySpawner.get_children():
+        if node is not EnemyBullet:
+            continue
+
+        var bullet: EnemyBullet = node
+        if bullet.speed < 1.0:
+            bullet._start_dying()
+            await Utilities.wait(0.1)
+
+
 func _ready() -> void:
+    _stop_bullets()
+    
     sprite_2d.texture = Utilities.load_image_from_path("res://PassiveMod/boss_world_passive/stop_node/area.png")
     sprite_2d.scale = Vector2.ZERO
     animation_player.play("expand")
