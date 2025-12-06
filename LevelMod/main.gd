288a8050f5d50a789e1988e686a166f8e40e14ff
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-06 12:02:08 -0700

    Added comments

--- LevelMod/main.gd
+++ LevelMod/main.gd
@@ -2,35 +2,35 @@ extends BaseMod
 
 
 func init_mod() -> void:
-	print("Init level mod")
-	
-	# Define a new level
-	var def: LevelDef = LevelDef.new({
-		"description": "The poison swamp level.",
-		"icon": "res://LevelMod/level-swamp.png",
-		# The "key" should be unique
-		"key": "swamp",
-		# The "level_path"
-		"level_path": "res://LevelMod/level/Level.tscn",
-		# Used to position this level on the map select screen
-		"map": {
-			# Which direction the map will be in relation to the ranomdize button
-			# Possible values are: top, bottom, left, right
-			"focus": ["left"],
-			# Actual icon to show on the map
-			"icon": "res://LevelMod/map-swamp.png",
-			# Position of the icon on the map
-			"position": {
-				"x": 25,
-				"y": 470
-			}
-		},
-		"title": "Swamp"
-	})
-	
-	# Add level to the existing list
-	DefHandler.add_level(def)
-	
-	# Add an entry for "Slime" to the node pool
-	# This allows the SlimeEnemy to be spawned and tracked within the NodePool
-	NodePool.scenes.set("Slime", load("res://LevelMod/slime/SlimeEnemy.tscn"))
+    print("Init level mod")
+    
+    # Define a new level
+    var def: LevelDef = LevelDef.new({
+        "description": "The poison swamp level.",
+        "icon": "res://LevelMod/level-swamp.png",
+        # The "key" should be unique
+        "key": "swamp",
+        # The "level_path"
+        "level_path": "res://LevelMod/level/Level.tscn",
+        # Used to position this level on the map select screen
+        "map": {
+            # Which direction the map will be in relation to the ranomdize button
+            # Possible values are: top, bottom, left, right
+            "focus": ["left"],
+            # Actual icon to show on the map
+            "icon": "res://LevelMod/map-swamp.png",
+            # Position of the icon on the map
+            "position": {
+                "x": 25,
+                "y": 470
+            }
+        },
+        "title": "Swamp"
+    })
+    
+    # Add level to the existing list
+    DefHandler.add_level(def)
+    
+    # Add an entry for "Slime" to the node pool
+    # This allows the SlimeEnemy to be spawned and tracked within the NodePool
+    NodePool.scenes.set("Slime", load("res://LevelMod/slime/SlimeEnemy.tscn"))
--- LevelMod/poison/Poison.gd
+++ LevelMod/poison/Poison.gd
@@ -5,19 +5,21 @@ extends Node2D
 
 
 func _on_pausable_timer_timeout() -> void:
-    shape_cast_2d.force_shapecast_update()
-    for i: int in shape_cast_2d.get_collision_count():
-        var body: CharacterBody2D = shape_cast_2d.get_collider(i)
-
-        if not (body is Enemy or body is Player):
-            continue
-        
-        if body is Enemy:
-            var atk: Attack = Attack.new(5.0)
-            atk.add_effect(PoisonDebuff.new(0.2))
-            var enemy: Enemy = body
-            enemy.take_damage(atk)
-        else:
-            var atk: Attack = Attack.new(2.0)
-            var player: Player = body
-            player.take_damage(atk)
+	shape_cast_2d.force_shapecast_update()
+	for i: int in shape_cast_2d.get_collision_count():
+		var body: CharacterBody2D = shape_cast_2d.get_collider(i)
+		
+		if not (body is Enemy or body is Player):
+			continue
+		
+		if body is Enemy:
+			# Damages enemies for 5 and has a 20% chance to apply the poison debuff
+			var atk: Attack = Attack.new(5.0)
+			atk.add_effect(PoisonDebuff.new(0.2))
+			var enemy: Enemy = body
+			enemy.take_damage(atk)
+		else:
+			# Damages the player for 2
+			var atk: Attack = Attack.new(2.0)
+			var player: Player = body
+			player.take_damage(atk)
