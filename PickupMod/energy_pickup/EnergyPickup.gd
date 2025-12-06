e6e8ff7fda399b343554ff42a95172a74d83912d
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-06 12:52:29 -0700

    Added comments

--- PickupMod/energy_pickup/EnergyPickup.gd
+++ PickupMod/energy_pickup/EnergyPickup.gd
@@ -1,22 +1,26 @@
 extends Pickup
 
 
+# Called when this node is freed and re-added to the NodePool
 func reset() -> void:
-    super.reset()
+	super.reset()
+	
+	# Reset modified values
+	value = 1.0
+	speed = 500.0
+	catch_squared_distance = 16.0
 
-    value = 1.0
-    speed = 500.0
-    catch_squared_distance = 16.0
 
-
+# Called when the player gets this pickup
 func on_pickup() -> void:
-    super.on_pickup()
-    
-    print("Picked up energy")
-    
-    for player: Player in Utilities.get_all_players():
-        for _i: int in 10:
-            for weapon: Weapon in player.get_weapon_nodes():
-                weapon.attack()
-            
-            await Utilities.wait(0.1)
+	super.on_pickup()
+	
+	print("Picked up energy")
+	
+	# Trigger every weapon to attack, with a small delay
+	for player: Player in Utilities.get_all_players():
+		for _i: int in 10:
+			for weapon: Weapon in player.get_weapon_nodes():
+				weapon.attack()
+			
+			await Utilities.wait(0.1)
--- PickupMod/main.gd
+++ PickupMod/main.gd
@@ -5,17 +5,21 @@ const ENERGY_KEY: String = "energy_pickup"
 
 
 func init_mod() -> void:
-    print("Init pickup mod")
-    
-    NodePool.scenes[ENERGY_KEY] = load("res://PickupMod/energy_pickup/EnergyPickup.tscn")
-    
-    PickupHandler.drop_handlers.append(func(enemy: Enemy) -> Pickup:
-        var can_spawn: bool = randf_range(0.0, 100.0) <= Stats.luck * 5.0
-        if not can_spawn:
-            return null
-        
-        print("Dropped energy")
-        var energy_pickup: Pickup = NodePool.create_node(ENERGY_KEY)
-        energy_pickup.global_position = enemy.global_position
-        return energy_pickup
-    )
+	print("Init pickup mod")
+	
+	# Add to node pool
+	NodePool.scenes[ENERGY_KEY] = load("res://PickupMod/energy_pickup/EnergyPickup.tscn")
+	
+	# Add function that will be triggered when an enemy dies
+	PickupHandler.drop_handlers.append(func(enemy: Enemy) -> Pickup:
+		# 5% chance to drop, increases by 5% per luck upgrade
+		var can_spawn: bool = randf_range(0.0, 100.0) <= Stats.luck * 5.0
+		if not can_spawn:
+			return null
+		
+		print("Dropped energy")
+		# Create or get instance of this node from the NodePool
+		var energy_pickup: Pickup = NodePool.create_node(ENERGY_KEY)
+		energy_pickup.global_position = enemy.global_position
+		return energy_pickup
+	)
