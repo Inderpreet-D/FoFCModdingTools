1f9185c3074c242c9e5b565548df263d4e6524f7
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-06 12:07:43 -0700

    Added comments

--- NodeMod/main.gd
+++ NodeMod/main.gd
@@ -2,13 +2,19 @@ extends BaseMod
 
 
 func init_mod() -> void:
-    print("Init node mod")
-    
-    var num_nodes: int = 15
-    var scene: PackedScene = load("res://NodeMod/node/Node.tscn")
-    for j: int in 3:
-        for i: int in num_nodes:
-            var instance: CanvasLayer = scene.instantiate()
-            @warning_ignore("unsafe_method_access")
-            instance.setup(num_nodes, i, j)
-            ModLoader.add_child(instance)
+	print("Init node mod")
+	
+	var num_nodes: int = 15
+	var scene: PackedScene = load("res://NodeMod/node/Node.tscn")
+	for j: int in 3:
+		for i: int in num_nodes:
+			var instance: CanvasLayer = scene.instantiate()
+			
+			# Do initial setup for this node
+			@warning_ignore("unsafe_method_access")
+			instance.setup(num_nodes, i, j)
+			
+			# Add to tree
+			ModLoader.add_child(instance)
+	
+	print("Added nodes from node mod")
--- NodeMod/node/Node.gd
+++ NodeMod/node/Node.gd
@@ -16,47 +16,49 @@ var starting_shift: float
 var alpha: float
 
 
+# Set initial variables
 func setup(num_nodes: int, i: int, j: int) -> void:
-    texture_offset = texture_offset.rotated((TAU / num_nodes) * i)
-    texture_offset = texture_offset.rotated(j * (PI / 3))
-    starting_texture_offset = texture_offset
-    
-    speed *= [1, -1][j % 2]
-    starting_speed = speed
-    
-    rotation_speed *= (1.0 + ((j + 1) / 3.0)) * [1, -1][j % 2]
-    starting_rotation_speed = rotation_speed
-    
-    shift = j * 50.0
-    starting_shift = shift
-    
-    alpha = [0.5, 0.3, 0.1][j]
+	texture_offset = texture_offset.rotated((TAU / num_nodes) * i)
+	texture_offset = texture_offset.rotated(j * (PI / 3))
+	starting_texture_offset = texture_offset
+	
+	speed *= [1, -1][j % 2]
+	starting_speed = speed
+	
+	rotation_speed *= (1.0 + ((j + 1) / 3.0)) * [1, -1][j % 2]
+	starting_rotation_speed = rotation_speed
+	
+	shift = j * 50.0
+	starting_shift = shift
+	
+	alpha = [0.5, 0.3, 0.1][j]
 
 
+# Animates the node
 func _process(delta: float) -> void:
-    var shift_offset: Vector2 = texture_offset.normalized() * shift
-    texture_rect.position = start_position + texture_offset + shift_offset
-    texture_offset = texture_offset.rotated(delta * speed)
-    texture_rect.rotation += delta * rotation_speed
+	var shift_offset: Vector2 = texture_offset.normalized() * shift
+	texture_rect.position = start_position + texture_offset + shift_offset
+	texture_offset = texture_offset.rotated(delta * speed)
+	texture_rect.rotation += delta * rotation_speed
 
 
+# Called when the game is paused
 func _on_pause_started() -> void:
-    texture_offset *= 0.5
-    speed *= 0.4
-    rotation_speed *= -1.5
+	texture_offset *= 0.5
+	speed *= 0.4
+	rotation_speed *= -1.5
 
 
+# Called when the game is not paused
 func _on_pause_ended() -> void:
-    texture_offset = starting_texture_offset
-    speed = starting_speed
-    rotation_speed = starting_rotation_speed
-    shift = starting_shift
+	texture_offset = starting_texture_offset
+	speed = starting_speed
+	rotation_speed = starting_rotation_speed
+	shift = starting_shift
 
 
 func _ready() -> void:
-    print("Added new node from node mod")
-    
-    SceneSwitcher.pause_started.connect(_on_pause_started)
-    SceneSwitcher.pause_ended.connect(_on_pause_ended)
-    
-    texture_rect.self_modulate.a = alpha
+	SceneSwitcher.pause_started.connect(_on_pause_started)
+	SceneSwitcher.pause_ended.connect(_on_pause_ended)
+	
+	texture_rect.self_modulate.a = alpha
