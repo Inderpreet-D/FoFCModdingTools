9f4394aa929effca6e96a1411da1b5a76b425083
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-06 13:55:26 -0700

    Added comments

--- SoundMod/main.gd
+++ SoundMod/main.gd
@@ -1,12 +1,18 @@
 extends BaseMod
 
 
+const SOUND_NAME: String = "mod/pop"
+
+
 func init_mod() -> void:
-    print("Init sound mod")
-    
-    AudioManager.add_sound("Priority", "mod/pop", -10.0, "res://SoundMod/pop-423717.mp3")
-    
-    EventBus.pickup_collected.connect(func(_pickup: Pickup) -> void:
-        print("Playing mod sound")
-        AudioManager.play("mod/pop")
-    )
+	print("Init sound mod")
+	
+	# Register sound with AudioManager
+	# Define with bus, name, volume, and path
+	AudioManager.add_sound("Priority", SOUND_NAME, -10.0, "res://SoundMod/pop-423717.mp3")
+	
+	# Play the sound when a pickup gets collected
+	EventBus.pickup_collected.connect(func(_pickup: Pickup) -> void:
+		print("Playing mod sound")
+		AudioManager.play(SOUND_NAME)
+	)
