2acd4f48a847d31dddf722688abcdd8f392adb7d
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-06 00:18:24 -0700

    Added comments

--- BackgroundMod/FlashbangerBackground.gd
+++ BackgroundMod/FlashbangerBackground.gd
@@ -1,28 +1,45 @@
-class_name FlashbangerBackground extends DefaultBackground
+class_name FlashbangerBackground extends Background
 
 
+# Called by registered signal when an enemy dies
 func _on_enemy_dead(enemy: Enemy) -> void:
-    var explosion: Explosion = NodePool.create_node("Explosion")
-    explosion.global_position = enemy.global_position
-    explosion.damage = 2.0
-    explosion.max_radius = 1030.0
-    explosion.expansion_rate_modifier = 20.0 / 1.5
-    Passives.modify_projectile(explosion)
-    
-    if is_instance_valid(explosion.get_parent()):
-        return
-    
-    var bullet_holder: Node = _player.get_tree().get_first_node_in_group("bullets")
-    bullet_holder.call_deferred("add_child", explosion)
+	# Get an instance of an "Explosion" from the NodePool
+	var explosion: Explosion = NodePool.create_node("Explosion")
+	
+	# Center the explosion on the enemy
+	explosion.global_position = enemy.global_position
+	
+	# Arbitrary, but low damage and large radius
+	explosion.damage = 2.0
+	explosion.max_radius = 1030.0
+	
+	# Pretty fast size increase
+	explosion.expansion_rate_modifier = 20.0 / 1.5
+	
+	# This lets any passives that the player has affect the explosion parameters
+	Passives.modify_projectile(explosion)
+	
+	# If the explosion instance still is mounted then we cannot re-add
+	# it to the tree or the game will crash
+	if is_instance_valid(explosion.get_parent()):
+		return
+	
+	# Add the explosion to the scene tree
+	var bullet_holder: Node = _player.get_tree().get_first_node_in_group("bullets")
+	bullet_holder.call_deferred("add_child", explosion)
 
 
+# Called when the background is first added to the player
 func assign(player: Player) -> void:
-    super.assign(player)
-    
-    EnemySpawner.enemy_dead.connect(_on_enemy_dead)
+	super.assign(player)
+	
+	# Add signal connection
+	EnemySpawner.enemy_dead.connect(_on_enemy_dead)
 
 
+# Called when the background is removed from the player
 func unassign() -> void:
-    super.unassign()
-    
-    EnemySpawner.enemy_dead.disconnect(_on_enemy_dead)
+	super.unassign()
+	
+	# Remove signal connection
+	EnemySpawner.enemy_dead.disconnect(_on_enemy_dead)
--- BackgroundMod/main.gd
+++ BackgroundMod/main.gd
@@ -2,23 +2,25 @@ extends BaseMod
 
 
 func init_mod() -> void:
-    print("Init background mod now")
-    
-    var def: BackgroundDef = BackgroundDef.new({
-        "all_weapons": false,
-        "description": ["Watch your eyes!"],
-        "icon": "res://assets/sprites/player/character-default.png",
-        "key": "FlashbangerBackground",
-        "title": "Flashbanger",
-        "weapons": [
-            "pistol",
-            "fire_magic",
-            "rpg",
-        ]
-    })
-    
-    # Add new background
-    DefHandler.add_background(def)
-    
-    # Register FlashbangerBackground class, just based on path
-    ClassRegistry.register("FlashbangerBackground", "res://BackgroundMod/FlashbangerBackground.gd")
+	print("Init background mod")
+	
+	# Define a new background
+	var def: BackgroundDef = BackgroundDef.new({
+		"all_weapons": false,
+		"description": ["Watch your eyes!"],
+		"icon": "res://assets/sprites/player/character-default.png",
+		"key": "FlashbangerBackground",
+		"title": "Flashbanger",
+		"weapons": [
+			"pistol",
+			"fire_magic",
+			"rpg",
+		]
+	})
+	
+	# Add background to list of existing backgrounds
+	DefHandler.add_background(def)
+	
+	# Register FlashbangerBackground class, based on path
+	# This is required so that the background can be correctly instantiated when chosen
+	ClassRegistry.register("FlashbangerBackground", "res://BackgroundMod/FlashbangerBackground.gd")
