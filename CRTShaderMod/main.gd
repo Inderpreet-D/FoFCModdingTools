7b8ad874c55b7292f92c29629e3d86f45048ce30
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-06 00:22:59 -0700

    Added comments, renamed

--- CRTShaderMod/main.gd
+++ CRTShaderMod/main.gd
@@ -2,8 +2,11 @@ extends BaseMod
 
 
 func init_mod() -> void:
-    print("Init CRT shader mod")
-    
-    var scene: PackedScene = load("res://CRTShaderMod/node/ShaderOverlay.tscn")
-    var instance: CanvasLayer = scene.instantiate()
-    ModLoader.add_child(instance)
+	print("Init CRT shader mod")
+	
+	# Load and instantiate the shader
+	var scene: PackedScene = load("res://CRTShaderMod/node/CRTShaderOverlay.tscn")
+	var instance: CanvasLayer = scene.instantiate()
+	
+	# Add the shader to the tree
+	ModLoader.add_child(instance)
