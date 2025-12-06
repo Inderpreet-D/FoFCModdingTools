6283c0e66a2bc2efb89903f24738d1e33f30d633
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-06 00:48:32 -0700

    Added comments

--- FastModLoaderMod/main.gd
+++ FastModLoaderMod/main.gd
@@ -2,10 +2,13 @@ extends BaseMod
 
 
 func init_mod() -> void:
-    print("Init fast mod loader mod")
-    
-    var node: ModLoaderUI = ModLoader.get_tree().root.get_node("/root/ModLoaderUI")
-    node.wait_delay = 0.0
-    node.end_delay = 0.0
-    
-    print("Fast mod loader done")
+	print("Init fast mod loader mod")
+	
+	# Finds the node that does the mod loading
+	var node: ModLoaderUI = ModLoader.get_tree().root.get_node("/root/ModLoaderUI")
+	
+	# Removes the artificial delay that is used for debugging issues while loading mods
+	node.wait_delay = 0.0
+	node.end_delay = 0.0
+	
+	print("Fast mod loader done")
