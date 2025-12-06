c636869d1c11353942e6c472493acbf681514d66
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-06 00:40:29 -0700

    Added comments

--- CursorMod/main.gd
+++ CursorMod/main.gd
@@ -2,21 +2,23 @@ extends BaseMod
 
 
 func init_mod() -> void:
-    print("Init cursor mod now")
-    
-    # Godot will throw an error when using load() to load this image since it is in a mod
-    # This still throws a warning but that is fine
-    var cursor_texture: Image = Image.load_from_file("res://CursorMod/cursor.png")
-    Input.set_custom_mouse_cursor(cursor_texture)
-    
-    for _i: int in 3:
-        Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
-        await Utilities.wait(0.2)
-        
-        Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-        await Utilities.wait(0.2)
-    
-    print("Adding cursor mod node")
-    var node_scene: PackedScene = load("res://CursorMod/node/Node.tscn")
-    var node_instance: Node = node_scene.instantiate()
-    ModLoader.add_child(node_instance)
+	print("Init cursor mod")
+	
+	# Godot will throw an error when using load() to load this image since it is in a mod
+	# This still throws a warning but that is fine
+	var cursor_texture: Image = Image.load_from_file("res://CursorMod/cursor.png")
+	Input.set_custom_mouse_cursor(cursor_texture)
+	
+	# Flashing animation
+	for _i: int in 3:
+		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+		await Utilities.wait(0.2)
+		
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		await Utilities.wait(0.2)
+	
+	# Instance and add node to scene
+	print("Adding cursor mod node")
+	var node_scene: PackedScene = load("res://CursorMod/node/Node.tscn")
+	var node_instance: Node = node_scene.instantiate()
+	ModLoader.add_child(node_instance)
--- CursorMod/node/Node.gd
+++ CursorMod/node/Node.gd
@@ -1,27 +1,37 @@
 extends Node
 
 
+# Basically the same as a normal timer but will be paused when the
+# game becomes paused and also works with small timeouts
 @onready var pausable_timer: PausableTimer = %PausableTimer
 
 
+# Blinks the cursor on and off
+func _play_animation() -> void:
+	if not InputManager.is_on_menu():
+		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+		return
+	
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	await Utilities.wait(0.1)
+	
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	await Utilities.wait(0.1)
+	
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	await Utilities.wait(0.1)
+
+
 func _on_pausable_timer_timeout() -> void:
-    pausable_timer.stop()
-    
-    if InputManager.is_on_menu():
-        Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-        await Utilities.wait(0.1)
-        
-        Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
-        await Utilities.wait(0.1)
-        
-        Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-        await Utilities.wait(0.1)
-    else:
-        Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
-    
-    pausable_timer.delay = randf_range(1.0, 3.0)
-    pausable_timer.start()
+	# Stop the timer to allow an animation to play
+	pausable_timer.stop()
+	
+	await _play_animation()
+	
+	# Restart timer with random timeout
+	pausable_timer.delay = randf_range(1.0, 3.0)
+	pausable_timer.start()
 
 
 func _ready() -> void:
-    print("Added cursor mod node")
+	print("Added cursor mod node")
