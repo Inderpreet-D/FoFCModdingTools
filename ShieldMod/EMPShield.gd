69d69aab9ea7f52a69f8d0ae7291d564b0cc0f0c
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-06 13:17:22 -0700

    Added comments

--- ShieldMod/EMPShield.gd
+++ ShieldMod/EMPShield.gd
@@ -1,34 +1,40 @@
 class_name EMPShield extends Shield
 
 
+# Called when the shield is instantiated
 func _init() -> void:
-    type = "EMPShield"
+	# Needs to match the class name
+	type = "EMPShield"
+	
+	super._init(ShieldStat._2)
 
-    super._init(ShieldStat._2)
 
-
+# Called when the shield is broken
 func on_break() -> void:
-    super.on_break()
-    
-    # Async func that will be called and done after the shield is un-equipped
-    # await will not work in on_break function for shields (by design)
-    var break_func: Callable = func() -> void:
-        for node: Node in EnemySpawner.get_children():
-            if node is not EnemyBullet:
-                continue
-            
-            var bullet: EnemyBullet = node
-            bullet.speed = 0.0
-            bullet.damage = 0.0
-            bullet.rotation_speed = 10.0
-
-        for node: Node in EnemySpawner.get_children():
-            if node is not EnemyBullet:
-                continue
-            
-            var bullet: EnemyBullet = node
-            if bullet.speed < 1.0:
-                bullet._start_dying()
-                await Utilities.wait(0.1)
-    
-    break_func.call()
+	super.on_break()
+	
+	# Async func that will be called and done after the shield is un-equipped
+	# await will not work in on_break function for shields (by design)
+	# so make it a Callable and call that instead
+	var break_func: Callable = func() -> void:
+		for node: Node in EnemySpawner.get_children():
+			if node is not EnemyBullet:
+				continue
+			
+			# Stop bullets, make them do no damage, and make them spin
+			var bullet: EnemyBullet = node
+			bullet.speed = 0.0
+			bullet.damage = 0.0
+			bullet.rotation_speed = 10.0
+		
+		# Remove each bullet one by one with a small delay
+		for node: Node in EnemySpawner.get_children():
+			if node is not EnemyBullet:
+				continue
+			
+			var bullet: EnemyBullet = node
+			if bullet.speed < 1.0:
+				bullet._start_dying()
+				await Utilities.wait(0.1)
+	
+	break_func.call()
--- ShieldMod/main.gd
+++ ShieldMod/main.gd
@@ -2,15 +2,22 @@ extends BaseMod
 
 
 func init_mod() -> void:
-    print("Init shield mod")
-    
-    var def: ShieldDef = ShieldDef.new({
-        "description": "Stops projectiles when <broken>",
-        "icon": "res://ShieldMod/sprite.png",
-        "key": "EMPShield",
-        "tier": 1,
-        "title": "EMP Shield"
-    })
-    DefHandler.add_shield(def)
-    
-    ClassRegistry.register("EMPShield", "res://ShieldMod/EMPShield.gd")
+	print("Init shield mod")
+	
+	# Define new shield
+	var def: ShieldDef = ShieldDef.new({
+		"description": "Stops projectiles when <broken>",
+		"icon": "res://ShieldMod/sprite.png",
+		# The "key" property needs to match a class name
+		"key": "EMPShield",
+		# The "tier" property determines what level of the "Shield Smith" library upgrade is needed for this shield to drop
+		# A value of 0 means the shield can always drop
+		"tier": 0,
+		"title": "EMP Shield"
+	})
+	
+	# Add the shield to the existing list
+	DefHandler.add_shield(def)
+	
+	# Allow the shield class to be instantiated later
+	ClassRegistry.register("EMPShield", "res://ShieldMod/EMPShield.gd")
