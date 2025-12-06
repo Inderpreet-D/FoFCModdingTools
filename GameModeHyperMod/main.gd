2b08a4eaaa660c3be9947e45a9fb30558a141e26
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-06 10:49:24 -0700

    Added comments

--- GameModeHyperMod/main.gd
+++ GameModeHyperMod/main.gd
@@ -2,15 +2,18 @@ extends BaseMod
 
 
 func init_mod() -> void:
-    print("Init game mode mod")
-    
-    var def: GameModeDef = GameModeDef.new({
-        "description": "1 minute run with the same amount of bosses and enemies as the standard mode",
-        "icon": "res://assets/sprites/ui/game_mode/game_mode_normal.png",
-        "key": "hyper_mod",
-        "scene": "res://GameModeHyperMod/mode/PlayHyper.tscn",
-        "title": "Hyper Mode"
-    })
-    
-    # Add new game mode
-    DefHandler.add_game_mode(def)
+	print("Init hyper game mode mod")
+	
+	# Define a new game mode
+	var def: GameModeDef = GameModeDef.new({
+		"description": "1 minute run with the same amount of bosses and enemies as the standard mode",
+		"icon": "res://assets/sprites/ui/game_mode/game_mode_normal.png",
+		# The "key" does not need to refer to anything
+		"key": "hyper_mod",
+		# The "scene" has to point to the path of the scene that will control this game mode
+		"scene": "res://GameModeHyperMod/mode/PlayHyper.tscn",
+		"title": "Hyper Mode"
+	})
+	
+	# Add new game mode to existing list
+	DefHandler.add_game_mode(def)
--- GameModeHyperMod/mode/PlayHyper.gd
+++ GameModeHyperMod/mode/PlayHyper.gd
@@ -1,28 +1,37 @@
 extends PlayBase
 
 
+# Used to modify each wave of the level that was chosen
 func modify_wave(wave: EnemyWave) -> EnemyWave:
-    var modified: EnemyWave = super.modify_wave(wave)
-
-    if modified.condition == EnemyWave.NextCondition.KillsCond:
-        modified.kills_needed = floori(modified.kills_needed / 4.0)
-        return modified
-
-    modified.time_needed /= 20.0
-    return modified
-
-
-func _modify_config(config: LevelConfig) -> void :
-    super._modify_config(config)
-
-    config._spawn_capacity *= 4.0
-    config.normal_capacity_change *= 3.0
-    config.empty_capacity_change *= 3.0
+	var modified: EnemyWave = super.modify_wave(wave)
+	
+	if modified.condition == EnemyWave.NextCondition.KillsCond:
+		# Decrease the number of kills needed to finish this wave by a factor of 4
+		modified.kills_needed = floori(modified.kills_needed / 4.0)
+	else:
+		# Make the wave take 20 times less time as normal
+		modified.time_needed /= 20.0
+	
+	return modified
 
 
-func _ready() -> void :
-    print("Hyper game mode readied")
-    
-    super._ready()
+# Used to modify the existing config for the level that was chosen
+func _modify_config(config: LevelConfig) -> void:
+	super._modify_config(config)
+	
+	# Sets initial capacity which controls the maximum number of enemies that can spawn
+	config._spawn_capacity *= 4.0
+	
+	# These control how much the capacity should increase by after each wave
+	# Empty is only used when there is no remaining capacity
+	config.normal_capacity_change *= 3.0
+	config.empty_capacity_change *= 3.0
 
-    timer.max_time_seconds = 1 * 60
+
+func _ready() -> void:
+	print("Hyper game mode ready")
+	
+	super._ready()
+	
+	# Controls how long the game should last
+	timer.max_time_seconds = 1 * 60
