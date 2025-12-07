12df15576c351971ed9a43b26c33b2ef4c257770
Inderpreet Dhillon <4023685+Inderpreet-D@users.noreply.github.com>
2025-12-07 02:44:49 -0700

    Update main.gd

--- SoundMod/main.gd
+++ SoundMod/main.gd
@@ -5,14 +5,18 @@ const SOUND_NAME: String = "mod/pop"
 
 
 func init_mod() -> void:
-	print("Init sound mod")
-	
-	# Register sound with AudioManager
-	# Define with bus, name, volume, and path
-	AudioManager.add_sound("Priority", SOUND_NAME, -10.0, "res://SoundMod/pop-423717.mp3")
-	
-	# Play the sound when a pickup gets collected
-	EventBus.pickup_collected.connect(func(_pickup: Pickup) -> void:
-		print("Playing mod sound")
-		AudioManager.play(SOUND_NAME)
-	)
+    print("Init sound mod")
+    
+    # Register sound with AudioManager
+    # Define with bus, name, volume, and path
+    AudioManager.add_sound({
+        name = SOUND_NAME,
+        volume = -10.0,
+        path = "res://SoundMod/pop-423717.mp3"
+    })
+    
+    # Play the sound when a pickup gets collected
+    EventBus.pickup_collected.connect(func(_pickup: Pickup) -> void:
+        print("Playing mod sound")
+        AudioManager.play(SOUND_NAME)
+    )
