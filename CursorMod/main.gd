4dfeb464047a692404323eae066a959485eec087
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-10 23:49:54 -0700

    Added more settings for debugging

--- CursorMod/main.gd
+++ CursorMod/main.gd
@@ -1,8 +1,21 @@
 extends BaseMod
 
 
+const MOD_NAME: String = "CursorMod"
+
+
+func _update_mod_settings() -> void:
+	ModLoader.config.update_setting(MOD_NAME, ModConfigSetting.new({
+		id = "should-blink",
+		name = "Should the cursor occasionally blink?",
+		default_value = true
+	}))
+
+
 func init_mod() -> void:
 	print("Init cursor mod")
+	
+	_update_mod_settings()
 	
 	# Godot will throw an error when using load() to load this image since it is in a mod
 	# This still throws a warning but that is fine
@@ -11,8 +24,29 @@ func init_mod() -> void:
 	Input.set_custom_mouse_cursor(cursor_texture, Input.CURSOR_ARROW, image_size / 2.0)
 	ProjectSettings.set_setting("display/mouse_cursor/tooltip_position_offset", image_size + Vector2(10.0, 10.0))
 	
-	# Instance and add node to scene
-	print("Adding cursor mod node")
-	var node_scene: PackedScene = load("res://CursorMod/node/Node.tscn")
-	var node_instance: Node = node_scene.instantiate()
-	ModLoader.add_child(node_instance)
+	# Called when mod settings have been loaded, happens after all the init_mod functions are called
+	ModLoader.mod_config_loaded.connect(func() -> void:
+		# Get setting
+		var should_blink: bool = ModLoader.config.get_setting_value(MOD_NAME, "should-blink")
+		
+		# Name of existing node
+		var node_name: String = "%sBlinkNode" % MOD_NAME
+		
+		# Remove exising node if it exists
+		var existing_node: Node = ModLoader.get_node_or_null("/root/%s" % node_name)
+		if existing_node:
+			existing_node.queue_free()
+			ModLoader.remove_child(existing_node)
+		
+		if not should_blink:
+			print("Blinking node removed")
+			return
+		
+		# Instance and add node to scene
+		print("Adding cursor mod node")
+		var node_scene: PackedScene = load("res://CursorMod/node/Node.tscn")
+		var node_instance: Node = node_scene.instantiate()
+		node_instance.name = node_name
+		ModLoader.add_child(node_instance, true)
+		print("Blinking node added")
+	)
--- ShieldMod/main.gd
+++ ShieldMod/main.gd
@@ -1,23 +1,54 @@
 extends BaseMod
 
 
+const MOD_NAME: String = "ShieldMod"
+
+
+func _update_mod_settings() -> void:
+	ModLoader.config.update_setting(MOD_NAME, ModConfigSetting.new({
+		id = "tier",
+		name = "Level of Shield Smith required, 0 means the shield can always drop",
+		default_value = 0,
+		options = [0, 1, 2]
+	}))
+	
+	ModLoader.config.update_setting(MOD_NAME, ModConfigSetting.new({
+		id = "title",
+		name = "Shield name",
+		default_value = "EMP Shield",
+		options = [
+			"EMP Shield",
+			"Electro-magnetic Pulse Shield",
+			"Pulse Shield",
+			"Mod Shield"
+		]
+	}))
+
+
 func init_mod() -> void:
 	print("Init shield mod")
 	
-	# Define new shield
-	var def: ShieldDef = ShieldDef.new({
-		"description": "Stops projectiles when <broken>",
-		"icon": "res://ShieldMod/sprite.png",
-		# The "key" property needs to match a class name
-		"key": "EMPShield",
-		# The "tier" property determines what level of the "Shield Smith" library upgrade is needed for this shield to drop
-		# A value of 0 means the shield can always drop
-		"tier": 0,
-		"title": "EMP Shield"
-	})
-	
-	# Add the shield to the existing list
-	DefHandler.add_shield(def)
+	_update_mod_settings()
 	
 	# Allow the shield class to be instantiated later
 	ClassRegistry.register("EMPShield", "res://ShieldMod/EMPShield.gd")
+	
+	ModLoader.mod_config_loaded.connect(func() -> void:
+		var selected_tier: int = ModLoader.config.get_setting_value(MOD_NAME, "tier")
+		var selected_title: String = ModLoader.config.get_setting_value(MOD_NAME, "title")
+		
+		# Define new shield
+		var def: ShieldDef = ShieldDef.new({
+			"description": "Stops projectiles when <broken>",
+			"icon": "res://ShieldMod/sprite.png",
+			# The "key" property needs to match a class name
+			"key": "EMPShield",
+			# The "tier" property determines what level of the "Shield Smith" library upgrade is needed for this shield to drop
+			# A value of 0 means the shield can always drop
+			"tier": selected_tier,
+			"title": selected_title
+		})
+		
+		# Add the shield to the existing list, will override any existing one with the same key
+		DefHandler.add_shield(def)
+	)
