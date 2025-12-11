9b934063b26e197989340a4aba2fee76d7ac2e83
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2025-12-10 20:52:06 -0700

    Fixed cursor settings

--- CursorMod/main.gd
+++ CursorMod/main.gd
@@ -32,7 +32,9 @@ func init_mod() -> void:
 	# Godot will throw an error when using load() to load this image since it is in a mod
 	# This still throws a warning but that is fine
 	var cursor_texture: Image = Image.load_from_file("res://CursorMod/cursor.png")
-	Input.set_custom_mouse_cursor(cursor_texture)
+	var image_size: Vector2 = Vector2(36.0, 28.0)
+	Input.set_custom_mouse_cursor(cursor_texture, Input.CURSOR_ARROW, image_size / 2.0)
+	ProjectSettings.set_setting("display/mouse_cursor/tooltip_position_offset", image_size + Vector2(10.0, 10.0))
 	
 	# Instance and add node to scene
 	print("Adding cursor mod node")
