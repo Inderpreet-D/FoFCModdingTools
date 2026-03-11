43a34a2376644b68e7ba9cb7b0f732afcd342ba3
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2026-03-11 14:07:16 -0600

    Added item loading from json

--- ActiveMod/active_manager/ActiveManager.gd
+++ ActiveMod/active_manager/ActiveManager.gd
@@ -1,8 +1,7 @@
-extends Node
+class_name ActiveManager extends Node
 
 
-# TODO Add a static dictionary and func to add overrides / extensions
-
+static var item_info: Dictionary = {}
 var _active: bool = false
 var _items: Array[BaseActiveItem] = []
 
@@ -12,12 +11,28 @@ func _ready() -> void:
 	EventBus.player_died.connect(_on_player_died)
 
 
-func _get_item_instance(background: Background) -> BaseActiveItem:
-	var scene: PackedScene = load("res://ActiveMod/base_active_item/BaseActiveItem.tscn")
+func _get_scene(background: Background) -> PackedScene:
+	var script: Variant = background.get_script()
+	if not script:
+		return null
 	
-	# TODO Get from static dictionary
-	if background is DefaultBackground:
-		scene = load("res://ActiveMod/active_items/healing/HealingActiveItem.tscn")
+	var as_script: Script = script
+	var background_key: String = as_script.get_global_name()
+	
+	if not item_info.has(background_key):
+		print("[ActiveMod] no item for background \"%s\"" % background_key)
+		return null
+	
+	var info: Dictionary = item_info.get(background_key)
+	var scene_path: String = info.get("scene")
+	return load(scene_path)
+
+
+func _get_item_instance(background: Background) -> BaseActiveItem:
+	var scene: PackedScene = _get_scene(background)
+	
+	if not scene:
+		return null
 	
 	return scene.instantiate()
 
@@ -33,7 +48,11 @@ func _on_game_started() -> void:
 	
 	for player: Player in Utilities.get_all_players():
 		var item: BaseActiveItem = _get_item_instance(player.background)
+		if not item:
+			continue
+		
 		item.setup(player)
+		
 		_items.append(item)
 		player.add_child(item)
 
@@ -67,3 +86,32 @@ func _process(_delta: float) -> void:
 			continue
 		
 		item.activate()
+
+
+# Can be used by other mods to add / overwrite items for specific backgrounds
+static func add_item(background_key: String, info: Dictionary) -> void:
+	if background_key.length() == 0:
+		print("[ActiveMod] unable to add item with invalid background key")
+		return
+	
+	if not info.has("name"):
+		print("[ActiveMod] unable to add item for background \"%s\", name is missing" % background_key)
+		return
+	
+	if not info.has("scene"):
+		print("[ActiveMod] unable to add item for background \"%s\", scene is missing" % background_key)
+		return
+	
+	if item_info.has(background_key):
+		print("[ActiveMod] overwriting item for background \"%s\"" % background_key)
+		return
+	
+	item_info.set(background_key, info)
+
+
+static func load_items_from_json() -> void:
+	var dict: Dictionary = Utilities.read_json_dict("res://ActiveMod/actives.json")
+	
+	for background_key: String in dict.keys():
+		var info_dict: Dictionary = dict.get(background_key)
+		add_item(background_key, info_dict)
--- ActiveMod/main.gd
+++ ActiveMod/main.gd
@@ -9,27 +9,33 @@ func _add_input_events() -> void:
 	var action_name: String = "use_active_item"
 	InputMap.add_action(action_name)
 	
-	var keyboard_event: InputEventKey = InputEventKey.new()
-	keyboard_event.keycode = DisplayServer.keyboard_get_keycode_from_physical(Key.KEY_F)
-	InputMap.action_add_event(action_name, keyboard_event)
+	for key: Key in [Key.KEY_F, Key.KEY_J, Key.KEY_K]:
+		var keyboard_event: InputEventKey = InputEventKey.new()
+		keyboard_event.keycode = DisplayServer.keyboard_get_keycode_from_physical(key)
+		InputMap.action_add_event(action_name, keyboard_event)
 	
 	var controller_event: InputEventJoypadButton = InputEventJoypadButton.new()
 	controller_event.button_index = JOY_BUTTON_LEFT_SHOULDER
 	InputMap.action_add_event(action_name, controller_event)
 
 
-# TODO Load these from a JSON file
 func _modify_background_descriptions() -> void:
 	for key: String in DefHandler.backgrounds:
+		if not ActiveManager.item_info.has(key):
+			continue
+		
+		var info: Dictionary = ActiveManager.item_info.get(key)
+		var item_name: String = info.get("name")
 		var def: BackgroundDef = DefHandler.backgrounds[key]
-		var item_name: String = "healing"
-		def._description += "\n%s" % def.build_description(["+Starts with %s active item" % item_name])
+		def._description += "\n%s" % def.build_description(["+Starts with %s" % item_name])
 
 
 func init_mod() -> void:
 	print("Init active mod")
 	
 	_add_input_events()
+	
+	ActiveManager.load_items_from_json()
 	
 	_modify_background_descriptions()
 	
