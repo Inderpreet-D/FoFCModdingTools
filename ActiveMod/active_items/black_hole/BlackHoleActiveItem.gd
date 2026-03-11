63c41ae083c5bc4e7456fc8d86b42b2f3063e5d4
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2026-03-11 16:41:33 -0600

    Added black hole item

---
+++ ActiveMod/active_items/black_hole/BlackHoleActiveItem.gd
@@ -0,0 +1,19 @@
+extends BaseActiveItem
+
+
+func setup(player: Player) -> void:
+	_charge_time = 30.0
+	
+	super.setup(player)
+
+
+func activate() -> void:
+	super.activate()
+	
+	var black_hole_instance: BlackHole = NodePool.create_node("BlackHole")
+	black_hole_instance.life_time = 8.0
+	black_hole_instance.scale *= 20.0
+	black_hole_instance.suck_speed = 200.0
+	black_hole_instance.suck_acceleration = 15.0
+	black_hole_instance.global_position = _player.global_position
+	get_tree().get_first_node_in_group("bullets").add_child.call_deferred(black_hole_instance)
