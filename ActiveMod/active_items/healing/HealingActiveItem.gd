c601e73fe05162d87feb55655497e158927bb26f
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2026-03-11 14:07:29 -0600

    Tweaked healing item

--- ActiveMod/active_items/healing/HealingActiveItem.gd
+++ ActiveMod/active_items/healing/HealingActiveItem.gd
@@ -1,13 +1,38 @@
 extends BaseActiveItem
 
 
+var _activation_time: float
+var _heal_percent: float
+var _active: bool = false
+var _active_elapsed: float = 0.0
+
+
 func setup(player: Player) -> void:
-	super.setup(player)
+	_charge_time = 15.0
 	
-	_charge_time = 60.0
+	_activation_time = 4.0
+	_heal_percent = 0.25
+	
+	super.setup(player)
 
 
 func activate() -> void:
 	super.activate()
 	
-	_player.heal(0.5)
+	_active = true
+	_active_elapsed = 0.0
+
+
+func _process(delta: float) -> void:
+	super._process(delta)
+	
+	if not _active:
+		return
+	
+	_active_elapsed += delta
+	if _active_elapsed >= _activation_time:
+		_active = false
+		return
+	
+	if is_instance_valid(_player):
+		_player.heal((_heal_percent / _activation_time) * delta)
